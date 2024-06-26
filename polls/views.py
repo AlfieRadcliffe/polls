0817d2f2cfd346eb0ba683f7628d6a6c3fd3c6cc
AlfieRadcliffe <alfieradcliffe1234@gmail.com>
2024-06-26 14:39:34 +0100

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 from django.shortcuts import render, get_object_or_404
 from .models import Client
-from .utilities import send_sms
+# from .utilities import send_sms
 
 def index(request):
     # Your view logic here
